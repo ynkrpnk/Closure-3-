9831b425f2e753cc40bc04c88f6b5d9f069c8ac2
ynkrpnk <ankarpenko35@gmail.com>
2026-05-01 16:38:58 +0300

    done

---
+++ src/index.js
@@ -0,0 +1,26 @@
+"use strict";
+
+function createFilter(callback) {
+  return function (array) {
+    if (!Array.isArray(array)) return [];
+
+    const result = [];
+
+    for (let i = 0; i < array.length; i++) {
+      if (callback(array[i], i, array)) {
+        result.push(array[i]);
+      }
+    }
+
+    return result;
+  };
+}
+
+const arrNumb = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
+
+const getEvenNumbers = createFilter((el) => el % 2 === 0);
+console.log(getEvenNumbers(arrNumb));
+
+
+const getGreateTenNumbers = createFilter((el) => el > 10);
+console.log(getGreateTenNumbers(arrNumb));
