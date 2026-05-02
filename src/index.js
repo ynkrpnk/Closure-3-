47b0f0e2e0ca76f1da94d23a362fa0ffc9704132
ynkrpnk <ankarpenko35@gmail.com>
2026-05-02 23:59:24 +0300

    num

--- src/index.js
+++ src/index.js
@@ -21,6 +21,9 @@ const arrNumb = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15];
 const getEvenNumbers = createFilter((el) => el % 2 === 0);
 console.log(getEvenNumbers(arrNumb));
 
+ 
+let num;
+
 
 const getGreateTenNumbers = createFilter((el) => el > 10);
 console.log(getGreateTenNumbers(arrNumb));
